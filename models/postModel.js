db4c7e505702bcc623d614ff7d7063f52a4407d8
Joakim Andersson <joakim@ktv-sjobo.se>
2022-04-14 17:51:35 +0200

    Change connection string Cant get Heruko to work!

--- models/postModel.js
+++ models/postModel.js
@@ -1,52 +1,5 @@
 const mongoose = require('mongoose');
-
-var options = {
-  useMongoClient: true,
-  useNewUrlParser: true, 
-  useUnifiedTopology: true, 
-  serverApi: ServerApiVersion.v1
-};
-
-var options2 = {
-  //useMongoClient: true,
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-};
-
-/*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://herokuapp:h5hMoXiYrpnU9k3r@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
-
-const username = "herokuapp";
-const password = "h5hMoXiYrpnU9k3r";
-const cluster = "cluster0.tuosg";
-const dbname = "post";
-const uri = "mongodb+srv://" + username + ":" + password + "@" + cluster + ".mongodb.net/" + dbname + "?retryWrites=true&w=majority";
-const uri2 = "mongodb+srv://herokuapp:h5hMoXiYrpnU9k3r@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority";
-
-/*
-mongoose.connect(
-  "mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
-*/
-
-const conn = mongoose.connect(uri, options);
-
-
-
-
+const conn = mongoose.connect('mongodb://localhost:27017/post');
 
 const Post = mongoose.model('Post', { title: String, text: String });
 
