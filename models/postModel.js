6646e8ecb082435c4cd2fd3abff927967400fc70
Joakim Andersson <joakim@ktv-sjobo.se>
2022-04-14 11:33:28 +0200

    New user

--- models/postModel.js
+++ models/postModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const conn = mongoose.connect('mongodb+srv://ejschallenge:PsPwW4mzMnSjFJFO@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority');
+const conn = mongoose.connect('mongodb+srv://ejschallenge:h5hMoXiYrpnU9k3r@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority');
 
 const Post = mongoose.model('Post', { title: String, text: String });
 
