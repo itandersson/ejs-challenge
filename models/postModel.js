4fc4eabc5a239cfe56b2439ebc0db68221875032
Joakim Andersson <joakim@ktv-sjobo.se>
2022-04-14 12:04:12 +0200

    Change connection string

--- models/postModel.js
+++ models/postModel.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
-const conn = mongoose.connect('mongodb+srv://ejschallenge:h5hMoXiYrpnU9k3r@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority');
+
+const uri = "mongodb+srv://herokuapp:h5hMoXiYrpnU9k3r@cluster0.tuosg.mongodb.net/post?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const Post = mongoose.model('Post', { title: String, text: String });
 
